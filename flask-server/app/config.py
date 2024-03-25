c0b47d36a0cdba19ae14e01851f7e06c3e349549
Kareem Ellis <kareemellis55@gmail.com>
2024-03-25 00:47:18 -0500

    Flask Server: Removed Unnecessary dependencies

--- flask-server/app/config.py
+++ flask-server/app/config.py
@@ -7,6 +7,5 @@ class Config(object):
     """Base Config Object"""
     DEBUG = False
     SECRET_KEY = os.environ.get('SECRET_KEY', 'UBDJh20dAJPDnnuKpzwOtduULATC8wLA')
-    UPLOAD_FOLDER = os.environ.get('UPLOAD_FOLDER')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace('postgres://', 'postgresql://')
     SQLALCHEMY_TRACK_MODIFICATIONS = False # This is just here to suppress a warning from SQLAlchemy as it will soon be removed)
--- flask-server/app/views.py
+++ flask-server/app/views.py
@@ -1,15 +1,8 @@
 import os
-from app import app, db, login_manager
-from flask import render_template, request, redirect, url_for, flash, session, abort, send_from_directory
-from flask_login import login_user, logout_user, current_user, login_required
-from werkzeug.utils import secure_filename
+from app import app, db
+from flask import request
 from app.models import *
 
-
-
-###
-# Routing for your application.
-###
 
 @app.route('/')
 def home():
